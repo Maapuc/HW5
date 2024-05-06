71517c5bd5f9a3a90ee69c535f17f02b43cff183
Nikolai <pojarka754@gmail.com>
2024-05-06 15:23:40 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" Задача 1 ");
+        int clientOS = 0;
+        int clientAndroid = 1;
+        int user = 1;
+        if (clientOS == user) {
+            System.out.println(" Установите версию приложения для iOS по cсылке ");
+        } else {
+            System.out.println(" Установите версию приложения для Android по cсылке ");
+        }
+        System.out.println(" Задача 2 ");
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear < 2015) {
+            System.out.println();
+        }
+
+        System.out.println(" Задача 3 ");
+        int years =2021;
 
     }
 }
